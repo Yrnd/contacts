797d60d92b4d97ad3fe4727d096b2c91976d9667
Yrnd <yaranday@yandex.ru>
2022-11-09 13:41:27 +0400

    js back

--- js/api.js
+++
@@ -1,5 +0,0 @@
-const reader = require('fs');
-
-module.exports = function loadContactList(){
-    return reader.readFileSync('resources/txt/contactList.txt');
-}
--- js/script.js
+++ js/script.js
@@ -1,43 +1,14 @@
-let idIncrement = 0;
-import * as fs from 'fs';
-
-let contact = {
-    "id": 0,
-    "name": " ",
-    "phone": " ",
-    "isElected": false,
-};
-
-const addButton = document.querySelector('#add_button');
-//Данные контакта
-const inputName = document.querySelector('#name');
-const phone = document.querySelector('#phone');
-const isElected = document.querySelector('#elected');
-
-//Обработка ивентов формы добавления
-isElected.addEventListener("change", function (){
-    contact[isElected.value] = isElected.checked;
-})
-addButton.addEventListener("click",function (){
-    if (inputName.value !== "" && phone.value !== "")
-        {
-            idIncrement++;
-            contact.id = idIncrement;
-            contact.name = inputName.value;
-            contact.phone = phone.value;
-            let json = JSON.stringify(contact);
-            console.log(json)
+//Список контактов
+export const contactList = {
+    data:[],
+    addContact: function (json){
+        this.data.push(json);
+    },
+    loadContactList: function (){
+        for (let contacts in contactList){
+        while (contacts != null){
+            console.log(1);
         }
-    else{
-        let error = document.createElement('div');
-        error.className = "alert";
-        error.innerHTML = "Вы не ввели все данные ! Повторите попытку";
-        document.querySelector(".wrapper").before(error);
-        sleep(700).then(() => {window.location.reload();})
     }
     }
-)
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+};
